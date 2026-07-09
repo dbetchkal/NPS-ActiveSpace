a59814e2c3b38f164f170bd341eeeea976368b45
Elliott Ruebush <eruebush@gmail.com>
2026-07-09 16:23:52 -0400

    TODO on constants

--- nps_active_space/utils/constants.py
+++ nps_active_space/utils/constants.py
@@ -4,3 +4,5 @@ import os
 # os.name on Windows — see https://docs.python.org/3/library/os.html#os.name
 OS_NAME_WINDOWS = "nt"
 IS_WINDOWS = os.name == OS_NAME_WINDOWS
+
+# TODO: put global unit conversion constants here (e.g. FEET_TO_METERS from project_setup.py)
